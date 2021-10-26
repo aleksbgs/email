d8dbdab18886de94b36bb5f4cc66db4f444e0568
Aleksandar Markovic <aleksandar.markovic@firstbeatmedia.com>
2021-10-26 21:29:09 +0200

    kafka sending email

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/smtp"
+)
 import "github.com/confluentinc/confluent-kafka-go/kafka"
 
 func main() {
 
-	//ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
-	//
-	//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
-	//
-	//adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
-	//
-	//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
-	//kafka keys
-	//Key
-	//DQF3PX3S424LV5A3
-	//
-	//Secret
-	//f62c0f8M8nQ2tQTxksKPFplfE/yNlGweddVCq0PINec8oqn5NxgNERi3k6okTceM
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "pkc-4ygn6.europe-west3.gcp.confluent.cloud:9092",
 		"security.protocol": "SASL_SSL",
@@ -36,12 +27,24 @@ func main() {
 
 	for {
 		msg, err := consumer.ReadMessage(-1)
-		if err == nil {
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else {
-			// The client will automatically try to recover from all errors.
+		if err != nil {
+
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			return
 		}
+		fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+
+		var message map[string]interface{}
+
+		json.Unmarshal(msg.Value, &message)
+
+		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", message["ambassador_revenue"], message["code"]))
+
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{message["ambassador_email"].(string)}, ambassadorMessage)
+
+		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", message["id"], message["admin_revenue"]))
+
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
 	}
 
 	consumer.Close()
